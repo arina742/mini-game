c9edf0918b1e56f08d034c2a0d25e8148c08feb8
arina742 <arinazav02@gmail.com>
2022-10-21 19:58:58 +0300

    Можно ввести свое имя и человека, с которым хотите проверить совместимость

--- src/Windows.java
+++ src/Windows.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Windows {
@@ -9,21 +11,30 @@ public class Windows {
         JFrame window = new JFrame("Узнай свою совместимость!");
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setVisible(true);
-        window.setSize(300,300);
+        window.setSize(300,200);
         window.getContentPane().setLayout(null);
         JButton button1 = new JButton();
         button1.setText("Результат");
         button1.setSize(125,50);
-        button1.setLocation(80, 200);
+        button1.setLocation(80, 100);
         window.add(button1); //процесс добавления кнопки
 
         window.setLayout(new BorderLayout());
-        window.add(new JLabel("Узнай свою совместимость"), BorderLayout.CENTER);
+        window.add(new JLabel("               Узнай свою совместимость"), BorderLayout.CENTER);
         window.setVisible(true);
         window.setLocationRelativeTo(null);
 
-
-
+        ActionListener AListener1 = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                int min = 50;
+                int max = 101;
+                double percents = Math.random() * (max - min) + min;
+                JOptionPane.showMessageDialog(null, "       Результат совместимости: " + Math.round(percents) + "%", "Совместимость", JOptionPane.PLAIN_MESSAGE );
+            }
+        };
+        button1.addActionListener(AListener1);
     }
 
     public static void showResult(JFrame window)
